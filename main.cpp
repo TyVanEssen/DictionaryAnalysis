dd40513c482a1cfe9b7df108accd26fa94550f60
TheHeion <38536319+TheHeion@users.noreply.github.com>
2020-12-01 16:02:48 -0700

    read number of lines in file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include <fstream>
 
-int main(int argc, char const argv[]){
-    ifstream inFile;
-    
+using namespace std;
+
+int main(int argc, char* argv[]){
+    ifstream inFile(argv[1]);
+
+    if (!inFile.is_open()){
+        cout << "[ ! ] File is not open" << endl;
+        exit;
+    }
+
+    int lines = 0;
+    string tmp;
+    while(getline(inFile, tmp)){
+        lines++;
+    }
+    cout << "Lines = " << lines << endl;
 }
